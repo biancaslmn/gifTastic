d57f5dc75cb90d5188b5c7d2a66500e539c0d715
Bianca Salomon <biancaslmn>
2018-10-04 15:53:41 -0400

    Third push

--- giphy.js
+++ giphy.js
@@ -1,60 +1,55 @@
-$(document).ready(function () {
+var dessertsArray = ["Chocolate Cake", "Creme Brule", "Strawberry Sundae", "Apple Pie"];
 
-    // Initial array of topics
-    var topics = ["White Tea", "Black Tea", "Green Tea", "Oolong Tea"];
-
-    function displayTopicInfo() {
-
-        var topic = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=QPelEyu2TpuV6EAM7q68fpYQT0v6XDTy";
-
-        //Get data from giphy website
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            $("#topics-view").text(JSON.stringify(response));
-        });
-    };
-    displayTopicInfo();
-
-    //Display data in buttons
-    function makeButtons() {
-
-        $("#buttons-view").empty();
-        //looping through topics
-        for (var i = 0; i < topics.length; i++) {
-
-            var gifButton = $("<button>");
-            // Adding a class of topic to our button
-            gifButton.addClass("topic");
-            // Adding a data-attribute
-            gifButton.attr("data-name", topics[i]);
-            console.log(topics[i]);
-            // Providing the initial button text
-            gifButton.text(topics[i]);
-            // Adding the button to the buttons-view div
-            $("#buttons-view").append(gifButton);
-        }
-        
+$(document).ready(function() {
+    for (var i = 0; i < dessertsArray.length; i++) {
+        $("#dessert-buttons").append("<button type='button' onclick='searchGif(\"" + dessertsArray[i] + "\")' class='btn btn-primary' value=' " + dessertsArray[i] + "'> " + dessertsArray[i] + " </button>");
     }
-    makeButtons();
-
-    $("#add-topic").on("click", function(event) {
-        event.preventDefault();
-
-        var movie = $("#topic-input").val().trim();
-
-       topics.push(topic);
-        console.log(topics);
-
-        makeButtons();
-    });
-
-
-      $(document).on("click", ".topic", displayTopicInfo);
-
-      makeButtons();
-
 });
 
+function athleteButtonClicked() {
+    var userInput = $('#dessert-input').val();
+    searchGif(userInput);
+}
+
+function submitButtonClicked() {
+    var userInput = $('#dessert-input').val();
+
+    if (userInput) {
+        $('#dessert-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
+
+function searchGif(gifName) {
+    $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=dc6zaTOxFJmzC',
+            type: 'GET',
+        })
+        .done(function(response) {
+            displayGif(response);
+        })
+}
+
+function displayGif(response) {
+    $('#desserts').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#desserts').append(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
+}
